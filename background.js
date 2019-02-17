d438e1d21884a031a03d504f75277e76a6c90d99
Nafeh Shoaib <nafehshoaib14@gmail.com>
2019-02-17 03:20:37 -0800

    Added Chrome extension files

---
+++ background.js
@@ -0,0 +1,62 @@
+var API_KEY = ''
+
+// chrome.runtime.onInstalled.addListener(function() {
+
+// });
+
+// http makes an HTTP request and calls callback with parsed JSON.
+var http = function (method, url, body, cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) { return; }
+      if (xhr.status >= 400) {
+        notify('API request failed');
+        console.log('XHR failed', xhr.responseText);
+        return;
+      }
+      cb(JSON.parse(xhr.responseText));
+    };
+    xhr.send(body);
+};
+
+http('GET', chrome.runtime.getURL('config.json'), '', function (obj) {
+    API_KEY = obj.key;
+    document.dispatchEvent(new Event('config-loaded'));
+});
+
+// detect makes a NLP API request with the API key.
+var articlate = function (type, article, cb) {
+    var url = 'https://language.googleapis.com/v1/documents:analyzeEntities?key=' + API_KEY;
+    var data = {
+      "document": {
+          article
+      },
+      encodingType: type
+    };
+    http('POST', url, JSON.stringify(data), cb);
+};
+
+chrome.contextMenus.create({
+    id: "articlate-article-link",
+    title: 'Articlate Article',
+    contexts: ['link'],
+    onclick: function (link) {
+        articlate('UTF16', link, function (data) {
+            var entities = data.responses;
+            createWordle(entities, function(wordle) {
+                displayPopup(wordle);
+            });
+        });
+    }
+});
+
+var createWordle = function(entitities, cb) {
+    cb(generateWordleImage());
+}
+
+var displayPopup = function (wordle) {
+    newWindow = window.open("", null, "height=200,width=400,status=yes,toolbar=no,menubar=no,location=no");
+    newWindow.document.write("<img src='" + wordle + "'/>");
+}
